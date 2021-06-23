02d8e2c29ffcb4d30ed3c67b9f556992fb24f86a
Kenny2github <kenny2minecraft@gmail.com>
2021-06-23 21:10:58 +0100

    NOT NULL some things

--- sql/commits.sql
+++ sql/commits.sql
@@ -2,13 +2,13 @@ CREATE TABLE IF NOT EXISTS /*_*/git_commits (
 	-- git commit hash - 40 hexits for sha1
 	sha1 char(40) binary PRIMARY KEY,
 	-- tree hash
-	tree char(40) binary,
-	-- parents (normal edits get one) - max 25(.6) parents
+	tree char(40) binary NOT NULL,
+	-- parents (normal edits get one) - max 25 parents
 	parents varchar(1024),
 	-- commit author
-	author varchar(255),
+	author varchar(255) NOT NULL,
 	-- committer (us for normal revs, author for git revs)
-	committer varchar(255),
+	committer varchar(255) NOT NULL,
 	-- extended commit message, if any
 	comment blob
 );
--- sql/revisions.sql
+++ sql/revisions.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS /*_*/git_revisions (
 	-- revision ID
 	rev_id integer unsigned,
 	-- git commit hash
-	git_commit char(40) binary,
+	git_commit char(40) binary NOT NULL,
 	-- keys
 	PRIMARY KEY(rev_id),
 	FOREIGN KEY(rev_id) REFERENCES /*_*/revision(rev_id) ON DELETE CASCADE,
