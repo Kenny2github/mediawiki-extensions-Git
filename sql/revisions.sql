c4aefa2251efb40e31fd89cb822e2eba10334612
Kenny2github <kenny2minecraft@gmail.com>
2021-06-23 18:49:16 +0100

    tabs, not spaces

--- sql/revisions.sql
+++ sql/revisions.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS /*_*/git_revisions (
-  -- revision ID
-  rev_id integer unsigned,
-  -- git commit hash
-  git_commit char(40) binary,
-  -- keys
-  PRIMARY KEY(rev_id),
-  FOREIGN KEY(rev_id) REFERENCES /*_*/revision(rev_id) ON DELETE CASCADE,
-  FOREIGN KEY(git_commit) REFERENCES /*_*/git_commits(sha1) ON DELETE CASCADE
+	-- revision ID
+	rev_id integer unsigned,
+	-- git commit hash
+	git_commit char(40) binary,
+	-- keys
+	PRIMARY KEY(rev_id),
+	FOREIGN KEY(rev_id) REFERENCES /*_*/revision(rev_id) ON DELETE CASCADE,
+	FOREIGN KEY(git_commit) REFERENCES /*_*/git_commits(sha1) ON DELETE CASCADE
 );
